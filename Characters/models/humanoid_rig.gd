0a42db2362fb9c4d08ecbb7929e6e5e7b359f7f7
Treygrr <phlasc@gmail.com>
2024-10-20 15:36:24 -0400

    export config, remove old scene

--- Characters/models/humanoid_rig.gd
+++
@@ -1,6 +0,0 @@
-extends Rig
-
-@onready var animation_tree:AnimationTree = %AnimationTree
-
-func _process(delta: float) -> void:
-	animation_tree.set("parameters/IWR/blend_position", Vector2(get_real_velocity().x, get_real_velocity().y))
